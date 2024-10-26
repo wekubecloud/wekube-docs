0ff1e69554e6eb43d1c0b01572b6f0eedeb2d8e7
wekubecloud <569273149@139.com>
2024-10-26 23:29:31 +0800

    Update make-image.ps1
    Add Docker deployment method

--- deploy/make-image.ps1
+++ deploy/make-image.ps1
@@ -3,3 +3,6 @@ docker push "registry.cn-heyuan.aliyuncs.com/wekube/wekube-docs:v0.9.21"
 
 # kubectl apply -f deploy/wekube-docs.yaml
 kubectl -n wekube-web get pod
+
+# docker stop wekube-docs && docker rm wekube-docs
+# docker run -d --name wekube-docs --restart=always -p 3100:80 registry.cn-heyuan.aliyuncs.com/wekube/wekube-docs:v0.9.21
